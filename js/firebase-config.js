19bd4bdd90ac8dc0792fab9470a59c9a98389257
Kronos.system <matthecarvalho@gmail.com>
2026-06-24 11:17:22 -0300

    Add Firebase configuration and initialization

---
+++ js/firebase-config.js
@@ -0,0 +1,14 @@
+// Configuração do Firebase
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "seu-projeto.firebaseapp.com",
+  databaseURL: "https://seu-projeto-default-rtdb.firebaseio.com",
+  projectId: "seu-projeto",
+  storageBucket: "seu-projeto.appspot.com",
+  messagingSenderId: "SEU_SENDER_ID",
+  appId: "SEU_APP_ID"
+};
+
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
